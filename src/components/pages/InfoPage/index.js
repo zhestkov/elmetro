035cf6ec300d2b2373cd608cda112b21f42d5c82
Andrew <ajestkov@gmail.com>
2018-09-21 00:03:16 +0300

    ButtonAntd style fix

--- src/components/pages/InfoPage/index.js
+++ src/components/pages/InfoPage/index.js
@@ -1,11 +1,16 @@
 // @flow
 import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
-
+import { ButtonAntd } from "../../Common/ButtonAntd";
 @inject("history")
 @observer
 export class InfoPage extends Component {
   render() {
-    return <div>Info Page</div>;
+    return (
+      <div>
+        Info Page
+        <ButtonAntd style={{ margin: "20px 10px" }}>Save</ButtonAntd>
+      </div>
+    );
   }
 }
